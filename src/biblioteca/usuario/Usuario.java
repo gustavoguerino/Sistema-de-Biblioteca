9336401f1c9e292164508e448ef3d3545a1a93ef
Gustavo Guerino <gustavoguerino@hotmail.com>
2016-07-07 17:43:06 -0300

    Adicionado novas classes

--- src/biblioteca/usuario/Usuario.java
+++ src/biblioteca/usuario/Usuario.java
@@ -1,5 +1,22 @@
 package biblioteca.usuario;
 
-public class Usuario {
+import biblioteca.acervo.ItemAcervo;
+
+public class Usuario implements InterfaceUsuario{
+
+	private int codUsuario;
+	private String nome, endereco, cpf;
+	public Usuario(){
+		
+	}
+	public void pagar() {
+		// TODO Auto-generated method stub
+		
+	}
+	public ItemAcervo escolherItemAcervo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
