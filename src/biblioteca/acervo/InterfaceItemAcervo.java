2e8e66033e152ddb728e46800ad121978a990f86
Mauro de Carvalho Gonçalves <mauro.dcg@gmail.com>
2016-08-06 04:04:32 -0300

    Métodos de "dataAluguel" e "dataEntrega"
    Métodos para manipular a data de aluguel e de entrega.

--- src/biblioteca/acervo/InterfaceItemAcervo.java
+++ src/biblioteca/acervo/InterfaceItemAcervo.java
@@ -1,8 +1,11 @@
 package biblioteca.acervo;
 
+import java.time.LocalDate;
+
 import biblioteca.usuario.Usuario;
 
 public interface InterfaceItemAcervo {
 	boolean estaPago();
-
+	public LocalDate diaAlugado(int ano, int mes, int dia);
+	public LocalDate diaDevolucao(int ano, int mes, int dia);
 }
--- src/biblioteca/acervo/ItemAcervo.java
+++ src/biblioteca/acervo/ItemAcervo.java
@@ -10,57 +10,48 @@ import java.time.*;
 public class ItemAcervo implements InterfaceItemAcervo {
 
 	private double custo;
-	private LocalDate dataAluguel;
-	private LocalDate dataDevolucao;
-	private LocalDate reservaLivro;
-	private Period diasDevolucao;
 	private boolean pago = true;
 	private boolean disponivel = true;
 
-
-	//Met�dos Getters and Setters, eles est�o ocultos, qualquer coisa clique no "+"
 	public double getCusto() {
 		return custo;
 	}
+
 	public void setCusto(double custo) {
 		this.custo = custo;
 	}
+
 	public boolean isPago() {
 		return pago;
 	}
+
 	public void setPago(boolean pago) {
 		this.pago = pago;
 	}
+
 	public boolean isDisponivel() {
 		return disponivel;
 	}
+
 	public void setDisponivel(boolean disponivel) {
 		this.disponivel = disponivel;
-	}
-	public LocalDate getDataAluguel() {
-		return dataAluguel;
-	}
-	public void setDataAluguel(LocalDate dataAluguel) {
-		this.dataAluguel = dataAluguel;
-	}
-	public LocalDate getDataDevolucao() {
-		return dataDevolucao;
-	}
-	public void setDataDevolucao(LocalDate dataDevolucao) {
-		this.dataDevolucao = dataDevolucao;
 	}
 
 	//Construtor
 	public ItemAcervo(){}
 
-
-
-
 	public boolean estaPago(){
-		
 		return pago;
 	}
-
-
+	
+	//Implementacao da logica de inserir uma data para alugar e devolver.
+	public LocalDate diaAlugado(int ano, int mes, int dia){
+		LocalDate alugado = LocalDate.of(ano, mes, dia);
+		return alugado;
+	}
+	public LocalDate diaDevolucao(int ano, int mes, int dia){
+		LocalDate alugado = LocalDate.of(ano, mes, dia);
+		return alugado;
+	}
 
 }
