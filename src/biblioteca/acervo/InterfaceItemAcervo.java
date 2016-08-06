e17aebb865c3fd7b025b3727c578a5027fdbe7da
Mauro de Carvalho Gonçalves <mauro.dcg@gmail.com>
2016-08-06 18:36:00 -0300

    Implementação de Métodos de Data
    Implementação de métodos de dataAluguel, dataDevolucao e dataEntrega.

--- src/biblioteca/acervo/InterfaceItemAcervo.java
+++ src/biblioteca/acervo/InterfaceItemAcervo.java
@@ -6,6 +6,7 @@ import biblioteca.usuario.Usuario;
 
 public interface InterfaceItemAcervo {
 	boolean estaPago();
-	public LocalDate diaAlugado(int ano, int mes, int dia);
-	public LocalDate diaDevolucao(int ano, int mes, int dia);
+	public LocalDate diaAlugado();
+	public LocalDate diaDevolucao();
+	public boolean dataEntrega(LocalDate diaDevolucao, LocalDate diaAlugado);
 }
--- src/biblioteca/acervo/ItemAcervo.java
+++ src/biblioteca/acervo/ItemAcervo.java
@@ -45,13 +45,24 @@ public class ItemAcervo implements InterfaceItemAcervo {
 	}
 	
 	//Implementacao da logica de inserir uma data para alugar e devolver.
-	public LocalDate diaAlugado(int ano, int mes, int dia){
-		LocalDate alugado = LocalDate.of(ano, mes, dia);
+	public LocalDate diaAlugado(){
+		LocalDate alugado = LocalDate.now();
 		return alugado;
 	}
-	public LocalDate diaDevolucao(int ano, int mes, int dia){
-		LocalDate alugado = LocalDate.of(ano, mes, dia);
+	public LocalDate diaDevolucao(){
+		//o usuario vai poder alugar o livro por 15 dias
+		LocalDate alugado = LocalDate.now().plusDays(15);
 		return alugado;
+	}
+	
+	//logica para verificar se o usuario está devendo ou nao
+	public boolean dataEntrega(LocalDate diaDevolucao, LocalDate diaAlugado){
+		boolean noprazo = false;
+		Period data_entrega = Period.between(diaAlugado, diaDevolucao);
+		if(data_entrega.getDays() > 0){
+			noprazo = true;
+		}
+		return noprazo;
 	}
 
 }
